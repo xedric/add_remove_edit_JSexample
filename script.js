258d4f30d8becfc2424de5c5034725acdd2d34d1
Niklas Mårdby <niklas.mardby@gmail.com>
2014-04-25 11:27:31 +0200

    finished with main features but BUG in edit
    all main features finished: add, remove and edit
    
    but, BUG in edit
    
    var first_name = ... doesnt work

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	var editIsOpen = false;
+
 	// react to when form is submitted
 	$("#add_item").click(function(event) {
     	event.preventDefault();
@@ -36,48 +38,17 @@ $(document).ready(function() {
 		});
 	});
 
-});
-
-	// react to when form is submitted
-	/*$( "#item_form" ).submit(function( event ) {
-		// stop form from being sent
+	// replace item with form when user clicks edit
+	$('#list_of_items').on('click', '.item_edit', function(event) {
+	    // stop load of link
 		event.preventDefault();
 
-		// get values from form
-		var first_name = $( "#first_name" ).val();
-		var last_name = $( "#last_name" ).val();
-
-		// if there was input in the textfields we add it as an item
-		if(first_name !== "" && last_name !== "")
+		if(!editIsOpen)
 		{
-			// empty the form
-			$('#item_form').trigger("reset");
-
-			// add element to list of items
-			var item = "<div class=\"item\"><span class=\"first_name\">" + first_name + "</span> <span class=\"last_name\">" + last_name + 
-				"</span><form class=\"item_remove\"><input type=\"submit\" value=\"Remove\" /></form>" + 
-				"<form class=\"item_edit\"><input type=\"submit\" value=\"Edit\" /></form>" + 
-				"</div>";
-			$( "#list_of_items" ).append( item );
-
-			// set focus back in the textbox
-			$( "#first_name" ).focus();
-		}
-
-		$( ".item_remove" ).submit(function( event ) {
-			// stop form from being sent
-			event.preventDefault();
-
-			// animation
-			$(this).parent().fadeOut( "slow", function() {
-				// animation finished -> remove element
-				$(this).remove();
-			});
-		});
-
-		$( ".item_edit" ).submit(function( event ) {
-			// stop form from being sent
-			event.preventDefault();
+			editIsOpen = true;
+			// get values from span !!!!!!!!!!!!!!BUGG!!!!!!!!!!!!!!!!!!!!
+			var first_name = $( ".first_name" ).html();
+			var last_name = $( ".last_name" ).html();
 
 			// create form as variable
 			var edit_form = "<form class=\"form_edit\">"+
@@ -87,30 +58,35 @@ $(document).ready(function() {
 								"</p><p>"+
 									"<label for=\"last_name\">Last name: </label>"+
 									"<input id=\"edit_last_name\" type=\"text\" name=\"last_name\" value=\""+last_name+"\"/>"+
-								"</p><input type=\"submit\" value=\"Save\" /></form>";
+								"</p><a id=\"save_item\" href=\"#\">Save</a></form>";
 
 			// replace div with form
 			$(this).parent().html(edit_form);
+		}
+	});
 
-			$( ".form_edit" ).submit(function( event ) {
-				// stop form from being sent
-				event.preventDefault();
+	// save form to item
+	$('#list_of_items').on('click', '#save_item', function(event) {
+	    // stop load of link
+		event.preventDefault();
 
-				// get values from form
-				var edit_first_name = $( "#edit_first_name" ).val();
-				var edit_last_name = $( "#edit_last_name" ).val();
+		// get values from form
+		var edit_first_name = $( "#edit_first_name" ).val();
+		var edit_last_name = $( "#edit_last_name" ).val();
 
-				// if there was input in the textfields we add it as an item
-				if(edit_first_name !== "" && edit_last_name !== "")
-				{
-					// add element to list of items
-					var edit_item = "<div class=\"item\"><span class=\"first_name\">" + edit_first_name + "</span> <span class=\"last_name\">" + edit_last_name + 
-						"</span><form class=\"item_remove\"><input type=\"submit\" value=\"Remove\" /></form>" + 
-						"<form class=\"item_edit\"><input type=\"submit\" value=\"Edit\" /></form>" + 
-						"</div>";
-					// replace form with div
-					$(this).parent().html(edit_item);
-				}
+		// if there was input in the textfields we add it as an item
+		if(edit_first_name !== "" && edit_last_name !== "")
+		{
+			// add element to list of items
+			var edit_item = "<span class=\"first_name\">" + edit_first_name + "</span> <span class=\"last_name\">" + edit_last_name + 
+						"</span><a class=\"item_remove\" href=\"#\">Remove</a><a class=\"item_edit\" href=\"#\">Edit</a>";
+
+			// replace form with div after animation
+			$(this).parent().fadeOut( "slow", function() {
+				// animation finished -> remove element
+				$(this).parent().html(edit_item);
+				editIsOpen = false;
 			});
-		});
-	});*/
+		}
+	});
+});
