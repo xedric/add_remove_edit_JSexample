f28515779f0ecf58861d0622cdafa27241a73e5b
Niklas Mårdby <niklas.mardby@gmail.com>
2014-04-25 14:26:19 +0200

    version 2 ready!
    now the new fields in the top form works as planned and also details,
    edit and remove link.
    
    version 3 should have one more animation...

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ $(document).ready(function() {
 								"<a class=\"item_edit\" href=\"#\">Edit</a>"+
 								"<a class=\"item_details\" href=\"#\">Details</a>" +
 							"</div>"+
-							"<div class=\"item_age_profession\">Age: "+age+" Profession: "+profession+"</div>"+
+							"<div class=\"item_age_profession\">Age: <span class=\"span_age\">"+age+
+								"</span> Profession: <span class=\"span_profession\">"+profession+"</span></div>"+
 						"</div>";
 			$( "#list_of_items" ).append( item );
 
@@ -61,7 +62,7 @@ $(document).ready(function() {
 		// animation
 		$(this).parent().fadeOut( "slow", function() {
 			// animation finished -> remove element
-			$(this).remove();
+			$(this).parent().remove();
 		});
 	});
 
@@ -73,11 +74,11 @@ $(document).ready(function() {
 		if(!editIsOpen)
 		{
 			editIsOpen = true;
-			// get values from span !!!!!!!!!!!!!!BUGG!!!!!!!!!!!!!!!!!!!!
-			//var first_name = $( ".span_first_name" ).html();
-			//var last_name = $( ".span_last_name" ).html();
-			var first_name = $(this).parent().children( ".span_first_name" ).html();
-			var last_name = $(this).parent().children( ".span_last_name" ).html();
+			// get values from span
+			var first_name 	= $(this).parent().children( ".span_first_name" ).html();
+			var last_name 	= $(this).parent().children( ".span_last_name" ).html();
+			var age 		= $(this).parent().parent().find( ".span_age" ).html();
+			var profession 	= $(this).parent().parent().find( ".span_profession" ).html();
 
 			// create form as variable
 			var edit_form = "<form class=\"form_edit\">"+
@@ -87,6 +88,12 @@ $(document).ready(function() {
 								"</p><p>"+
 									"<label for=\"last_name\">Last name: </label>"+
 									"<input id=\"edit_last_name\" type=\"text\" name=\"last_name\" value=\""+last_name+"\"/>"+
+								"</p><p>"+
+									"<label for=\"age\">Age: </label>"+
+									"<input id=\"edit_age\" type=\"text\" name=\"edit_age\" value=\""+age+"\"/>"+
+								"</p><p>"+
+									"<label for=\"profession\">Profession: </label>"+
+									"<input id=\"edit_profession\" type=\"text\" name=\"profession\" value=\""+profession+"\"/>"+
 								"</p><a id=\"save_item\" href=\"#\">Save</a></form>";
 
 			// replace div with form
@@ -100,15 +107,26 @@ $(document).ready(function() {
 		event.preventDefault();
 
 		// get values from form
-		var edit_first_name = $( "#edit_first_name" ).val();
-		var edit_last_name = $( "#edit_last_name" ).val();
+		var edit_first_name	 = $( "#edit_first_name" ).val();
+		var edit_last_name	 = $( "#edit_last_name" ).val();
+		var edit_age		 = $( "#edit_age" ).val();
+		var edit_profession	 = $( "#edit_profession" ).val();
 
 		// if there was input in the textfields we add it as an item
-		if(edit_first_name !== "" && edit_last_name !== "")
+		if(edit_first_name !== "" && edit_last_name !== "" && edit_age !== "" && edit_profession !== "")
 		{
 			// add element to list of items
-			var edit_item = "<span class=\"span_first_name\">" + edit_first_name + "</span> <span class=\"span_last_name\">" + edit_last_name + 
-						"</span><a class=\"item_remove\" href=\"#\">Remove</a><a class=\"item_edit\" href=\"#\">Edit</a>";
+			//var edit_item = "<span class=\"span_first_name\">" + edit_first_name + "</span> <span class=\"span_last_name\">" + edit_last_name + 
+			//			"</span><a class=\"item_remove\" href=\"#\">Remove</a><a class=\"item_edit\" href=\"#\">Edit</a>";
+			var edit_item = "<div class=\"first_row\">" +
+								"<span class=\"span_first_name\">" + edit_first_name + "</span> " +
+								"<span class=\"span_last_name\">" + edit_last_name + "</span>" +
+								"<a class=\"item_remove\" href=\"#\">Remove</a>"+
+								"<a class=\"item_edit\" href=\"#\">Edit</a>"+
+								"<a class=\"item_details\" href=\"#\">Details</a>" +
+							"</div>"+
+							"<div class=\"item_age_profession\">Age: <span class=\"span_age\">"+edit_age+
+								"</span> Profession: <span class=\"span_profession\">"+edit_profession+"</span></div>";
 
 			// replace form with div after animation
 			$(this).parent().fadeOut( "slow", function() {
